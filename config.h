7342ae37dd937166cff7e92c30962595e1974cf1
TheInvisibility <45173598+TheInvisibility@users.noreply.github.com>
2021-05-05 14:10:07 +0200

    Greek letters for bar

--- config.h
+++ config.h
@@ -38,7 +38,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6"/*, "7", "8", "9" */};
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6"/*, "7", "8", "9" */};
+static const char *tags[] = { "α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι"};
 
 static const Rule rules[] = {
 	/* xprop(1):
