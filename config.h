54aa55c0735d6787adbdf6f537618d095db5f46e
TheInvisibility <45173598+TheInvisibility@users.noreply.github.com>
2021-05-05 10:15:02 +0200

    Added screenshot shortcuts

--- config.h
+++ config.h
@@ -103,6 +103,8 @@ static const char *dmenustart[] = { "/home/aymeric/scripts/dmen.sh", NULL };
 static const char *slockcmd[] = { "slock", NULL };
 static const char *search[] = { "search", NULL };
 static const char *google[] = { "google", NULL };
+static const char *screenshotcmd[] = { "scrot -q 100", NULL };
+static const char *screenshotzonecmd[] = { "screenshot", NULL };
 
 static const char *brightnessupcmd[] = { "brightnessctl", "set", "5%+", NULL };
 static const char *brightnessdowncmd[] = { "brightnessctl", "set", "5%-", NULL };
@@ -119,12 +121,14 @@ static Key keys[] = {
 	{ MODKEY,             					XK_s,	     spawn,          {.v = search } },
 	{ MODKEY,             					XK_g,	     spawn,          {.v = google } },
 	{ MODKEY|ShiftMask,             XK_l,	     spawn,          {.v = slockcmd } },
+	{ MODKEY,             					XK_s,	     spawn,          {.v = screenshotcmd } },
+	{ MODKEY|ShiftMask,             XK_s,	     spawn,          {.v = screenshotzonecmd } },
 
-	{ 0,                            XF86MonBrightnessUp,  spawn,  {.v = brightnessupcmd } },
-	{ 0,                            XF86MonBrightnessDown,    spawn,  {.v = brightnessdowncmd } },
-	{ 0,                            XF86AudioRaiseVolume,   spawn,  {.v = volumeraisecmd } },
-	{ 0,                            XF86AudioLowerVolume,   spawn,  {.v = volumelowercmd } },
-	{ 0,                            XF86AudioMute,   spawn,  {.v = volumetogglecmd } },
+	{ 0,                         XF86MonBrightnessUp,  spawn,  {.v = brightnessupcmd } },
+	{ 0,                     XF86MonBrightnessDown,    spawn,  {.v = brightnessdowncmd } },
+	{ 0,                       XF86AudioRaiseVolume,   spawn,  {.v = volumeraisecmd } },
+	{ 0,                       XF86AudioLowerVolume,   spawn,  {.v = volumelowercmd } },
+	{ 0,                           	  XF86AudioMute,   spawn,  {.v = volumetogglecmd } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
