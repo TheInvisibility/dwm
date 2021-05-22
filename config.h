c70f45aee005e12a7a61adaaf01a44d23ef42ec7
TheInvisibility <45173598+TheInvisibility@users.noreply.github.com>
2021-05-22 17:00:58 +0200

    Bar padding

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -10,6 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static int smartgaps      	    = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Jetbrains Mono:size=11" };
 static const char dmenufont[]       = "Jetbrains Mono:size=11";
 
@@ -23,7 +25,7 @@ static const char dmenufont[]       = "Jetbrains Mono:size=11";
 //static const char col_cyan[]        = "#222222";
 static const unsigned int baralpha = 0xd0;
 //static const unsigned int baralpha = OPAQUE;
-static const unsigned int borderalpha = OPAQUE;
+static const unsigned int borderalpha = 0xd0;
 
 //static const char *colors[][3]      = {
 	/*               fg         bg         border   */
